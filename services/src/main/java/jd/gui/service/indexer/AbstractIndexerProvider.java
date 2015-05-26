bf1bce9c603aaff414ad17ab28f061c534c5eb0c
emmanue1 <emmanue1@users.noreply.github.com>
2015-05-26 19:44:14 +0200

    Fixes Java/Groovy dependencies

---
+++ services/src/main/java/jd/gui/service/indexer/AbstractIndexerProvider.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2008-2015 Emmanuel Dupuy
+ * This program is made available under the terms of the GPLv3 License.
+ */
+
+package jd.gui.service.indexer;
+
+import jd.gui.spi.Indexer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+import java.util.regex.Pattern;
+
+public abstract class AbstractIndexerProvider implements Indexer {
+
+    protected List<String> externalSelectors;
+    protected Pattern externalPathPattern;
+
+    /**
+     * Initialize "selectors" and "pathPattern" with optional external properties file
+     */
+    AbstractIndexerProvider() {
+        Properties properties = new Properties();
+        Class clazz = this.getClass();
+
+        try (InputStream is = clazz.getClassLoader().getResourceAsStream(clazz.getName().replace('.', '/') + ".properties")) {
+            if (is != null) {
+                properties.load(is);
+            }
+        } catch (IOException ignore) {
+        }
+
+        init(properties);
+    }
+
+    protected void init(Properties properties) {
+        String selectors = properties.getProperty("selectors");
+        externalSelectors = (selectors == null) ? null : Arrays.asList(selectors.split(","));
+
+        String pathRegExp = properties.getProperty("pathRegExp");
+        externalPathPattern = (pathRegExp == null) ? null : Pattern.compile(pathRegExp);
+    }
+
+    protected List<String> getExternalSelectors() { return externalSelectors; }
+    protected Pattern getExternalPathPattern() { return externalPathPattern; }
+
+    public String[] getSelectors() {
+        return (externalSelectors==null) ? null : externalSelectors.toArray(new String[externalSelectors.size()]);
+    }
+    public Pattern getPathPattern() { return externalPathPattern; }
+}
