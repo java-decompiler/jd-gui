37a61d21931d00bf45594cedef975fc74c43e4ee
emmanue1 <java.decompiler@gmail.com>
2019-03-14 00:03:41 +0100

    Update license headers

--- app/src/main/groovy/org/jd/gui/OsxApp.groovy
+++ app/src/main/groovy/org/jd/gui/OsxApp.groovy
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2008-2019 Emmanuel Dupuy.
+ * This project is distributed under the GPLv3 license.
+ * This is a Copyleft license that gives the user the right to use,
+ * copy and modify the code freely for non-commercial purposes.
+ */
+
 package org.jd.gui
 
 import com.apple.eawt.AppEvent
--- services/src/main/groovy/org/jd/gui/service/treenode/WarPackageTreeNodeFactoryProvider.groovy
+++ services/src/main/groovy/org/jd/gui/service/treenode/WarPackageTreeNodeFactoryProvider.groovy
@@ -1,6 +1,8 @@
 /*
- * Copyright (c) 2008-2015 Emmanuel Dupuy
- * This program is made available under the terms of the GPLv3 License.
+ * Copyright (c) 2008-2019 Emmanuel Dupuy.
+ * This project is distributed under the GPLv3 license.
+ * This is a Copyleft license that gives the user the right to use,
+ * copy and modify the code freely for non-commercial purposes.
  */
 
 package org.jd.gui.service.treenode
