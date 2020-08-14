a33fe61b6bc2cbcc349f4addc51d5617d42f0c5d
Tom Lafford <tom@laffyco.net>
2020-08-14 09:01:12 +0100

    Select previously selected term

--- app/src/main/java/org/jd/gui/view/MainView.java
+++ app/src/main/java/org/jd/gui/view/MainView.java
@@ -354,6 +354,8 @@ public class MainView<T extends JComponent & UriGettable> implements UriOpenable
         invokeLater(() -> {
             findPanel.setVisible(true);
             findComboBox.requestFocus();
+			// Selects previously searched term
+			findComboBox.getEditor().selectAll();
         });
     }
 
